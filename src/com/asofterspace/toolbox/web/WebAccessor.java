9ab17a4801f7730b43b6da67ff52696415e69566
Moya <moyaccercchi@hotmail.de>
2018-05-01 15:44:47 +0200

    Improve PUT behavior

--- src/com/asofterspace/toolbox/web/WebAccessor.java
+++ src/com/asofterspace/toolbox/web/WebAccessor.java
@@ -3,6 +3,7 @@ package com.asofterspace.toolbox.web;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
@@ -63,46 +64,51 @@ public class WebAccessor {
 	}
 
 	private static String getPutPost(String url, String messageBody, String requestKind) {
-		
+
 		try {
-	
 			URL urlAsURL = new URL(url);
-			
+
 			HttpURLConnection connection = (HttpURLConnection) urlAsURL.openConnection();
-			
+
 			connection.setRequestMethod(requestKind);
-			
+
 			// pretend to be a reasonable browser; in this case: firefox; see:
 			// https://stackoverflow.com/questions/13670692/403-forbidden-with-java-but-not-web-browser
 			// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/User-Agent/Firefox
 			connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:10.0) Gecko/20100101 Firefox/10.0");
 
 			if ("GET".equals(requestKind)) {
-				connection.connect();
 			} else {
+				connection.setDoOutput(true);
+				/*
 				String postData = URLEncoder.encode(messageBody, "UTF-8");
 				byte[] postDataBytes = postData.toString().getBytes("UTF-8");
 
 				connection.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
-				connection.setDoOutput(true);
 				connection.getOutputStream().write(postDataBytes);
+				*/
+				OutputStreamWriter out = new OutputStreamWriter(
+						connection.getOutputStream());
+				out.write(messageBody);
+				out.close();
 			}
-			
+			connection.connect();
+
 			InputStreamReader ireader = new InputStreamReader(connection.getInputStream());
 			BufferedReader reader = new BufferedReader(ireader);
-			
+
 			String nextLine = reader.readLine();
 			StringBuilder data = new StringBuilder();
-			
+
 			while (nextLine != null) {
 				data.append(nextLine);
 				nextLine = reader.readLine();
 			}
-			
+
 			reader.close();
-			
+
 			return data.toString();
-  
+
 		} catch (IOException e) {
 			System.out.println("There was an IOException in get for " + url + "\n" + e);
 			return "";
