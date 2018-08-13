97540c280fa8ded5dc11c385b66da69cee7ba742
Moya <moyaccercchi@hotmail.de>
2018-08-13 16:44:49 +0200

    Add utilities for handling dates and notifications

---
+++ src/com/asofterspace/toolbox/date/DateUtils.java
@@ -0,0 +1,29 @@
+package com.asofterspace.toolbox.date;
+
+import java.util.Date;
+
+/**
+ * Date-related utility class full of date-related utitlity stuff!
+ */
+
+public class DateUtils {
+
+    public static boolean isLessThanXDaysInTheFuture(Date date, int x) {
+
+        Date now = new Date();
+
+        // get the time difference between the (future) date and now
+        long diffTime = date.getTime() - now.getTime();
+
+        // if the future date is earlier than now, then the date is not in the future at all!
+        if (diffTime < 0) {
+            return false;
+        }
+
+        long diffDays = diffTime / (1000 * 60 * 60 * 24);
+
+        // the final result depends on whether the difference is less than x - or not (yet)
+        return diffDays < x;
+    }
+
+}
---
+++ src/com/asofterspace/toolbox/notification/NotificationUtils.java
@@ -0,0 +1,25 @@
+package com.asofterspace.toolbox.notification;
+
+/**
+ * This class provides utilities for handling notifications.
+ */
+
+public class NotificationUtils {
+
+    final public static String KEY_NOTIFICATION_ID = "NOTIFICATION_ID";
+
+    final public static String KEY_RESULT = "RESULT";
+
+    private static int latestNotification = 0;
+
+    /**
+     * Generates a UNIQUE notification id!
+     * @return A notification id
+     */
+    public static synchronized int generateNotificationId() {
+
+        latestNotification++;
+
+        return latestNotification;
+    }
+}
