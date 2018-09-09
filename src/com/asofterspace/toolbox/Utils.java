778f1d93dd777765bb671cd777b89f2c852b473e
Moya <moyaccercchi@hotmail.de>
2018-09-09 14:49:42 +0200

    Add small utlitiy function for transferring int to intth

--- src/com/asofterspace/toolbox/Utils.java
+++ src/com/asofterspace/toolbox/Utils.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 
 public class Utils {
 
-	public final static int TOOLBOX_VERSION_NUMBER = 8;
+	public final static int TOOLBOX_VERSION_NUMBER = 9;
 
     private static final char[] ECORE_UUID_LETTERS = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '_'};
 
@@ -43,5 +43,31 @@ public class Utils {
 		}
 
 		return sb.toString();
+	}
+	
+	/**
+	 * Takes a number, e.g. 2 or 6 or 11 or 42, and returns it as ordinal string, e.g. 2nd, 6th, 11th or 42nd
+	 */
+	public static String th(int i) {
+		
+		// 11, 12 and 13 are special - they end in 1, 2 and 3, but get th anyway
+		switch (i % 100) {
+			case 11:
+			case 12:
+			case 13:
+				return i + "th";
+		}
+		
+		// all others are simpler: if they end with 1 - st, if with 2 - nd, if with 3 - rd, else - th
+		switch (i % 10) {
+			case 1:
+				return i + "st";
+			case 2:
+				return i + "nd";
+			case 3:
+				return i + "rd";
+		}
+		
+		return i + "th";
 	}
 }
