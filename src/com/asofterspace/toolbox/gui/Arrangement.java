adb0957e8cad8688e7ec7d00ef913be9cf2eaec6
Moya <moyaccercchi@hotmail.de>
2018-09-09 12:08:23 +0200

    Improve Arrangement to fill space only when requested

--- src/com/asofterspace/toolbox/gui/Arrangement.java
+++ src/com/asofterspace/toolbox/gui/Arrangement.java
@@ -14,10 +14,27 @@ public class Arrangement extends GridBagConstraints {
 
 		super();
 
-		this.fill = GridBagConstraints.BOTH;
+		this.anchor = GridBagConstraints.CENTER;
+		
 		this.gridx = gridx;
 		this.gridy = gridy;
+		
 		this.weightx = weightx;
 		this.weighty = weighty;
+		
+		// only fill the available space if the component is supposed to take up more space!
+		if (weightx == 0.0) {
+			if (weighty == 0.0) {
+				this.fill = GridBagConstraints.NONE;
+			} else {
+				this.fill = GridBagConstraints.VERTICAL;
+			}
+		} else {
+			if (weighty == 0.0) {
+				this.fill = GridBagConstraints.HORIZONTAL;
+			} else {
+				this.fill = GridBagConstraints.BOTH;
+			}
+		}
 	}
 }
