7ad7d1d4797fb1ec276cd0220a9de9f3ce4ddc29
Moya <moyaccercchi@hotmail.de>
2018-09-09 15:34:41 +0200

    Add GUI utils class with utility functions for GUIs

---
+++ src/com/asofterspace/toolbox/gui/GuiUtils.java
@@ -0,0 +1,41 @@
+package com.asofterspace.toolbox.gui;
+
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Window;
+import javax.swing.JComponent;
+
+
+public class GuiUtils {
+
+	/**
+	 * Takes a created window - e.g. a JFrame or a JDialog - and shows it in the middle of the screen
+	 */
+	public static void centerAndShowWindow(Window window) {
+
+		// Center the window
+		window.setLocationRelativeTo(null);
+
+		// Stage everything to be shown
+		window.pack();
+
+		// Actually display the whole jazz
+		window.setVisible(true);
+	}
+
+	/**
+	 * Extends the width of a component in a vertical boxlayout to the maximum width of the container
+	 * Attention: This should only be called after all the content has been added to whatToMakeWide,
+	 * as it will set the current vertical size as maximum size, so if you add content to it later,
+	 * it will not be considered!
+	 */
+	public static void makeWide(JComponent whatToMakeWide) {
+
+		whatToMakeWide.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+		Dimension makeWide = whatToMakeWide.getPreferredSize();
+		makeWide.width = Integer.MAX_VALUE;
+		whatToMakeWide.setMaximumSize(makeWide);
+	}
+
+}
