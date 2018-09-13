b4f03b63781180982edaae9fe55534f451b193fe
Moya <moyaccercchi@hotmail.de>
2018-09-13 08:56:31 +0200

    Make GUI slightly more generic

--- src/com/asofterspace/toolbox/gui/GuiUtils.java
+++ src/com/asofterspace/toolbox/gui/GuiUtils.java
@@ -2,13 +2,35 @@ package com.asofterspace.toolbox.gui;
 
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.GraphicsEnvironment;
+import java.awt.Rectangle;
 import java.awt.Window;
 
 import javax.swing.JComponent;
+import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 
 public class GuiUtils {
+
+	/**
+	 * Maximize the window
+	 */
+	public static void maximizeWindow(JFrame window) {
+	
+		GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		
+		Rectangle bounds = env.getMaximumWindowBounds();
+		window.setMaximizedBounds(bounds);
+		
+		window.setSize((int) bounds.getWidth(), (int) bounds.getHeight());
+		
+		window.setPreferredSize(new Dimension((int) bounds.getWidth(), (int) bounds.getHeight()));
+		
+		// This should actually maximize the window, but for some reason does not work (reliably),
+		// so instead we do it manually in the lines above...
+		// window.setExtendedState(mainFrame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+	}
 
 	/**
 	 * Takes a created window - e.g. a JFrame or a JDialog - and shows it in the middle of the screen
---
+++ src/com/asofterspace/toolbox/gui/MainWindow.java
@@ -0,0 +1,43 @@
+package com.asofterspace.toolbox.gui;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+
+
+/**
+ * This can be used as the basis for any main window, yay!
+ */
+public abstract class MainWindow implements Runnable {
+
+	protected JFrame mainFrame;
+
+
+	@Override
+	public void run() {
+	
+		create();
+		
+		show();
+	}
+
+	public void create() {
+
+		JFrame.setDefaultLookAndFeelDecorated(false);
+		JDialog.setDefaultLookAndFeelDecorated(false);
+
+		// Create the window
+		mainFrame = new JFrame();
+		
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public void show() {
+
+		GuiUtils.centerAndShowWindow(mainFrame);
+	}
+
+	public JFrame getMainFrame() {
+		return mainFrame;
+	}
+
+}
