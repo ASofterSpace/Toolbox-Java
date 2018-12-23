7d33f06f1efd47fe24d73381e8e9762fd7164c4d
Moya <moyaccercchi@hotmail.de>
2018-12-23 16:10:40 +0100

    Add rudimentary (!) HTML, PHP and JavaScript highlighting support

---
+++ src/com/asofterspace/toolbox/codeeditor/PhpCode.java
@@ -0,0 +1,19 @@
+/**
+ * Unlicensed code created by A Softer Space, 2018
+ * www.asofterspace.com/licenses/unlicense.txt
+ */
+package com.asofterspace.toolbox.codeeditor;
+
+import javax.swing.JTextPane;
+
+
+public class PhpCode extends HtmlCode {
+
+	private static final long serialVersionUID = 1L;
+
+
+	public PhpCode(JTextPane editor) {
+
+		super(editor);
+	}
+}
