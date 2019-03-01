5a7011c9cd9945ccbc8edfcaa2e7c3831314fbb5
Tom Moya Schiller <moya@asofterspace.com>
2019-03-01 09:07:13 +0000

    Add basic coders for CSS, Python, Shell Scripts and XML

--- src/com/asofterspace/toolbox/codeeditor/CssCode.java
+++ src/com/asofterspace/toolbox/codeeditor/CssCode.java
@@ -4,12 +4,68 @@
  */
 package com.asofterspace.toolbox.codeeditor;
 
+import com.asofterspace.toolbox.utils.Callback;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.GraphicsEnvironment;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.swing.event.DocumentEvent;
 import javax.swing.JTextPane;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultEditorKit;
+import javax.swing.text.DefaultStyledDocument;
+import javax.swing.text.Element;
+import javax.swing.text.MutableAttributeSet;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.TabSet;
+import javax.swing.text.TabStop;
 
 
-public class CssCode extends JavaCode {
+public class CssCode extends Code {
 
 	private static final long serialVersionUID = 1L;
+
+	// all keywords of the C# language
+	private static final Set<String> KEYWORDS = new HashSet<>(Arrays.asList(
+		new String[] {"as", "assert", "break", "case", "catch", "const", "continue", "def", "default", "do", "else", "extends", "false", "finally", "for", "foreach", "goto", "if", "implements", "using", "in", "instanceof", "interface", "new", "null", "return", "super", "switch", "this", "throw", "throws", "trait", "true", "try", "var", "while"}
+	));
+
+	// all primitive types of the C# language and other stuff that looks that way
+	private static final Set<String> PRIMITIVE_TYPES = new HashSet<>(Arrays.asList(
+		new String[] {"bool", "char", "class", "double", "float", "enum", "int", "long", "abstract", "final", "package", "private", "protected", "public", "static", "void", "volatile", "synchronized"}
+	));
+
+	// all string delimiters of the C# language
+	private static final Set<Character> STRING_DELIMITERS = new HashSet<>(Arrays.asList(
+		new Character[] {'"', '\''}
+	));
+
+	// operand characters in the C# language
+	private static final Set<Character> OPERAND_CHARS = new HashSet<>(Arrays.asList(
+		new Character[] {';', ':', '.', ',', '{', '}', '(', ')', '[', ']', '+', '-', '/', '%', '<', '=', '>', '!', '&', '|', '^', '~', '*', '#'}
+	));
+
+	// start of multiline comments in the Java language
+	private static final String START_MULTILINE_COMMENT = "/*";
+
+	// end of multiline comments in the Java language
+	private static final String END_MULTILINE_COMMENT = "*/";
+
+	// are we currently in a multiline comment?
+	private boolean curMultilineComment;
+
+	private List<CodeLocation> functions = new ArrayList<>();
 
 
 	public CssCode(JTextPane editor) {
@@ -17,4 +73,224 @@ public class CssCode extends JavaCode {
 		super(editor);
 	}
 
+	@Override
+	public void insertString(int offset, String insertedString, AttributeSet attrs) {
+
+		int overrideCaretPos = insertedString.length();
+
+		// automagically close brackets that are being opened
+		switch (insertedString) {
+			case "{":
+				insertedString = "{}";
+				overrideCaretPos = 1;
+				break;
+			case "(":
+				insertedString = "()";
+				overrideCaretPos = 1;
+				break;
+			case "[":
+				insertedString = "[]";
+				overrideCaretPos = 1;
+				break;
+			case "\"":
+				insertedString = "\"\"";
+				overrideCaretPos = 1;
+				break;
+			case "'":
+				// replace ' with '' in code - but not in comment mode!
+				if (attrComment.equals(attrs)) {
+					break;
+				}
+				insertedString = "''";
+				overrideCaretPos = 1;
+				break;
+		}
+
+		super.insertString(offset, insertedString, attrs, overrideCaretPos);
+	}
+
+	// this is the main function that... well... highlights our text :)
+	@Override
+	void highlightText(int start, int length) {
+
+		functions = new ArrayList<>();
+
+		try {
+			int end = this.getLength();
+
+			String content = this.getText(0, end);
+
+			// set the entire document back to regular
+			this.setCharacterAttributes(0, end, attrRegular, true);
+
+			// TODO :: actually use the start and length passed in as arguments!
+			// (currently, they are just being ignored...)
+			start = 0;
+			end -= 1;
+
+			while (start <= end) {
+
+				// while we have a delimiter...
+				char curChar = content.charAt(start);
+				while (isDelimiter(curChar)) {
+
+					// ... check for a comment (which starts with a delimiter)
+					if (isCommentStart(content, start, end)) {
+						start = highlightComment(content, start, end);
+					} else {
+						// please highlight the delimiter in the process ;)
+						if (!Character.isWhitespace(curChar)) {
+							this.setCharacterAttributes(start, 1, attrReservedChar, false);
+						}
+					}
+
+					if (start < end) {
+
+						// jump forward and try again!
+						start++;
+
+					} else {
+						return;
+					}
+
+					curChar = content.charAt(start);
+				}
+
+				// now check what we have: a quoted string?
+				if (isStringDelimiter(curChar)) {
+
+					// then let's get that string!
+					start = highlightString(content, start, end);
+
+				} else {
+
+					// or any other token instead?
+					start = highlightOther(content, start, end);
+				}
+			}
+
+		} catch (BadLocationException e) {
+			// oops!
+		}
+	}
+
+	private boolean isCommentStart(String content, int start, int end) {
+
+		if (start + 1 > end) {
+			return false;
+		}
+
+		return START_MULTILINE_COMMENT.equals(content.substring(start, start + 2));
+	}
+
+	private int highlightComment(String content, int start, int end) {
+
+		String commentStart = content.substring(start, start + 2);
+
+		// apply multiline comment highlighting
+		int commentEnd = content.indexOf(END_MULTILINE_COMMENT, start + 2);
+
+		// the multiline comment has not been closed - let's comment out the rest of the document!
+		if (commentEnd == -1) {
+			commentEnd = end;
+		} else {
+			// +1 because of the length of END_MULTILINE_COMMENT itself
+			commentEnd += 1;
+		}
+
+		// apply multiline comment highlighting
+		this.setCharacterAttributes(start, commentEnd - start + 1, attrComment, false);
+
+		return commentEnd;
+	}
+
+	private int highlightString(String content, int start, int end) {
+
+		// get the string delimiter that was actually used to start this string (so " or ') to be able to find the matching one
+		String stringDelimiter = content.substring(start, start + 1);
+
+		// find the end of line - as we do not want to go further
+		int endOfLine = content.indexOf(EOL, start + 2);
+
+		if (endOfLine == -1) {
+			endOfLine = end;
+		}
+
+		// find the matching end of string
+		int endOfString = start;
+
+		while (true) {
+			endOfString = content.indexOf(stringDelimiter, endOfString + 1);
+
+			// if the end of string is actually escaped... well, then it is not an end of string yet, continue searching!
+			if ((endOfString == -1) || (content.charAt(endOfString - 1) != '\\')) {
+				break;
+			}
+		}
+
+		if (endOfString == -1) {
+			// the string is open-ended... go for end of line
+			endOfString = endOfLine;
+		} else {
+			// the string is not open-ended... so will the end marker or the line break be first?
+			endOfString = Math.min(endOfString, endOfLine);
+		}
+
+		this.setCharacterAttributes(start, endOfString - start + 1, attrString, false);
+
+		return endOfString + 1;
+	}
+
+	private int highlightOther(String content, int start, int end) {
+
+		int couldBeKeywordEnd = start + 1;
+
+		while (couldBeKeywordEnd <= end) {
+			if (isDelimiter(content.charAt(couldBeKeywordEnd))) {
+				break;
+			}
+			couldBeKeywordEnd++;
+		}
+
+		String couldBeKeyword = content.substring(start, couldBeKeywordEnd);
+
+		if (isKeyword(couldBeKeyword)) {
+			this.setCharacterAttributes(start, couldBeKeywordEnd - start, attrKeyword, false);
+		} else if (isPrimitiveType(couldBeKeyword)) {
+			this.setCharacterAttributes(start, couldBeKeywordEnd - start, attrPrimitiveType, false);
+		} else if (isAdvancedType(couldBeKeyword)) {
+			this.setCharacterAttributes(start, couldBeKeywordEnd - start, attrAdvancedType, false);
+		} else if (isAnnotation(couldBeKeyword)) {
+			this.setCharacterAttributes(start, couldBeKeywordEnd - start, attrAnnotation, false);
+		}
+
+		return couldBeKeywordEnd;
+	}
+
+	private boolean isDelimiter(char character) {
+		return Character.isWhitespace(character) || OPERAND_CHARS.contains(character);
+	}
+
+	private boolean isStringDelimiter(char character) {
+		return STRING_DELIMITERS.contains(character);
+	}
+
+	private boolean isKeyword(String token) {
+		return KEYWORDS.contains(token);
+	}
+
+	private boolean isPrimitiveType(String token) {
+		return PRIMITIVE_TYPES.contains(token);
+	}
+
+	private boolean isAdvancedType(String token) {
+		if (token.length() < 1) {
+			return false;
+		}
+		return Character.isUpperCase(token.charAt(0));
+	}
+
+	private boolean isAnnotation(String token) {
+		return token.startsWith("@");
+	}
 }
