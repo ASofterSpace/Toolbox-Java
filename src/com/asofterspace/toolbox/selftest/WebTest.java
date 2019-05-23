2722111d4e01527793c17237a8477d1951702a84
Tom Moya Schiller <moya@asofterspace.com>
2019-05-23 17:58:03 +0200

    Add two new (failing) tests
    The tests seem right... the toolbox needs to be fixed :)

--- src/com/asofterspace/toolbox/selftest/WebTest.java
+++ src/com/asofterspace/toolbox/selftest/WebTest.java
@@ -4,7 +4,10 @@
  */
 package com.asofterspace.toolbox.selftest;
 
+import com.asofterspace.toolbox.io.DefaultImageFile;
 import com.asofterspace.toolbox.io.Directory;
+import com.asofterspace.toolbox.io.File;
+import com.asofterspace.toolbox.io.JSON;
 import com.asofterspace.toolbox.test.Test;
 import com.asofterspace.toolbox.test.TestUtils;
 import com.asofterspace.toolbox.web.WebAccessor;
@@ -19,11 +22,22 @@ public class WebTest implements Test {
 	@Override
 	public void runAll() {
 
+		clearEverything();
+
 		startServer();
 
 		communicateWithServerTest();
 
+		getAdvancedJsonFromServerTest();
+
+		getFileTest();
+
 		stopServer();
+	}
+
+	public void clearEverything() {
+
+		WebAccessor.clearCache();
 	}
 
 	public void startServer() {
@@ -43,6 +57,76 @@ public class WebTest implements Test {
 
 		if (!"{\"foo\": \"bar\"}".equals(result)) {
 			TestUtils.fail("We tried to request a simple JSON file from our own server... and did not get it!");
+			return;
+		}
+
+		TestUtils.succeed();
+	}
+
+	public void getAdvancedJsonFromServerTest() {
+
+		TestUtils.start("Get Advanced JSON from Server");
+
+		server.addToWhitelist("json/advanced.json");
+
+		String result = WebAccessor.get("http://localhost:8081/json/advanced.json");
+
+		JSON jsonContent = new JSON(result);
+
+		if (!jsonContent.getString("foo").equals("")) {
+			TestUtils.fail("We stored {\"foo\": \"\"} in an advanced JSON file, then read the file - and did not get an empty string when querying for foo!");
+			return;
+		}
+
+		if (!jsonContent.getString("bar").equals("\"")) {
+			TestUtils.fail("We stored {\"bar\": \"\\\"\"} in a JSON file, then read the file - and did not get \" when querying for bar!");
+			return;
+		}
+
+		if (!jsonContent.get("blu").asString().equals("blubb")) {
+			TestUtils.fail("We stored {'blu': \"blubb\"} in a JSON file, then read the file - and did not get blubb when querying for blu!");
+			return;
+		}
+
+		if (!jsonContent.getString("newline").equals("\n")) {
+			TestUtils.fail("We stored {\"newline\": \"\\n\"} in an advanced JSON file, then read the file - and did not get \\n when querying for newline!");
+			return;
+		}
+
+		System.out.println(jsonContent.getString("leftout"));
+
+		if (!jsonContent.getString("leftout").equals("the Gänsefüßchen")) {
+			TestUtils.fail("We stored {leftout: 'the Gänsefüßchen'} in a JSON file, then read the file - and did not get the Gänsefüßchen when querying for leftout!");
+			return;
+		}
+
+		if (!jsonContent.getBoolean("otherbool").equals(false)) {
+			TestUtils.fail("We stored {\"otherbool\": false} in a JSON file, then read the file - and did not get false when querying for otherbool!");
+			return;
+		}
+
+		if (!jsonContent.getString("possible").equals("right?")) {
+			TestUtils.fail("We stored {\"possible\": \"right?\"} in an advanced JSON file, then read the file - and did not get right? when querying for possible!");
+			return;
+		}
+
+		TestUtils.succeed();
+	}
+
+	public void getFileTest() {
+
+		TestUtils.start("Get File from Server");
+
+		DefaultImageFile origImage = new DefaultImageFile(AllTests.IMAGE_TEST_DATA_PATH + "/qrtest_large_automask.png");
+
+		server.addToWhitelist("images/qrtest_large_automask.png");
+
+		File gotFile = WebAccessor.getFile("http://localhost:8081/images/qrtest_large_automask.png");
+
+		DefaultImageFile gotImage = new DefaultImageFile(gotFile);
+
+		if (!origImage.equals(gotImage)) {
+			TestUtils.fail("We tried to request an image file from our own server... and did not get it!");
 			return;
 		}
 
