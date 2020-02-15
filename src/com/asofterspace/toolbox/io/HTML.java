b67b5f08ccecc9f789df4df48744071c0455c5f7
Tom Moya Schiller <moya@asofterspace.com>
2020-02-15 19:40:16 +0100

    Add HTML escaping

---
+++ src/com/asofterspace/toolbox/io/HTML.java
@@ -0,0 +1,21 @@
+/**
+ * Unlicensed code created by A Softer Space, 2020
+ * www.asofterspace.com/licenses/unlicense.txt
+ */
+package com.asofterspace.toolbox.io;
+
+
+public class HTML {
+
+	/**
+	 * Takes in a string such as:
+	 * foo<bar
+	 * Returns a string such as:
+	 * foo&lt;bar
+	 * @param str  a string that possibly contains <, >, etc. signs
+	 * @return a string in which all such signs are escaped
+	 */
+	public static String escapeHTMLstr(Object strToEscape) {
+		return XML.escapeXMLstr(strToEscape);
+	}
+}
