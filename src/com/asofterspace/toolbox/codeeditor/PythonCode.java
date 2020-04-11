0adaa3be1754c0bf6a807d32574cae76de6c30fc
Tom Moya Schiller <moya@asofterspace.com>
2020-04-11 23:32:47 +0200

    Add elif to python highlighter

--- src/com/asofterspace/toolbox/codeeditor/PythonCode.java
+++ src/com/asofterspace/toolbox/codeeditor/PythonCode.java
@@ -22,7 +22,7 @@ public class PythonCode extends Code {
 
 	// all keywords of the C# language
 	private static final Set<String> KEYWORDS = new HashSet<>(Arrays.asList(
-		new String[] {"and", "as", "assert", "break", "case", "catch", "const", "continue", "def", "default", "do", "else", "extends", "false", "finally", "for", "foreach", "goto", "if", "implements", "import", "in", "instanceof", "interface", "new", "not", "null", "or", "return", "super", "switch", "this", "throw", "throws", "trait", "true", "try", "using", "var", "while", "with"}
+		new String[] {"and", "as", "assert", "break", "case", "catch", "const", "continue", "def", "default", "do", "elif", "else", "extends", "false", "finally", "for", "foreach", "goto", "if", "implements", "import", "in", "instanceof", "interface", "new", "not", "null", "or", "return", "super", "switch", "this", "throw", "throws", "trait", "true", "try", "using", "var", "while", "with"}
 	));
 
 	// all primitive types of the C# language and other stuff that looks that way
