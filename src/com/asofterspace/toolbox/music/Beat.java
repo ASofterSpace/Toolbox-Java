8f8cfe7e9de9eaa81c3e5e4900087471885e58e6
Tom Moya Schiller <moya@asofterspace.com>
2020-06-07 20:23:52 +0200

    Add fields to Beat object

--- src/com/asofterspace/toolbox/music/Beat.java
+++ src/com/asofterspace/toolbox/music/Beat.java
@@ -4,10 +4,55 @@
  */
 package com.asofterspace.toolbox.music;
 
+
 public class Beat {
 
+	// where in the sound data is the beat located?
 	private int position;
 
-	private int loudness;
+	// how long is this beat?
+	private int length;
 
+	// who loud is the entire beat overall?
+	private long loudness;
+
+	// how jiggly is the beat (that is, how many up/down and down/up reversals are there within it?)
+	private long jigglieness;
+
+
+	public Beat(int position) {
+		this.position = position;
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
+	public void setPosition(int position) {
+		this.position = position;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+	public long getLoudness() {
+		return loudness;
+	}
+
+	public void setLoudness(long loudness) {
+		this.loudness = loudness;
+	}
+
+	public long getJigglieness() {
+		return jigglieness;
+	}
+
+	public void setJigglieness(long jigglieness) {
+		this.jigglieness = jigglieness;
+	}
 }
