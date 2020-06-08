5d216757b6181d35c8b227e2582f4c2f6c8e7b56
Tom Moya Schiller <moya@asofterspace.com>
2020-06-08 19:07:44 +0200

    Add ColorRGB constructor based on longs

--- src/com/asofterspace/toolbox/images/ColorRGB.java
+++ src/com/asofterspace/toolbox/images/ColorRGB.java
@@ -89,6 +89,46 @@ public class ColorRGB {
 		this.a = (byte) a;
 	}
 
+	public ColorRGB(long r, long g, long b) {
+		this(r, g, b, 255l);
+	}
+
+	public ColorRGB(long r, long g, long b, long a) {
+
+		if (r > 255) {
+			r = 255;
+		}
+		if (r < 0) {
+			r = 0;
+		}
+
+		if (g > 255) {
+			g = 255;
+		}
+		if (g < 0) {
+			g = 0;
+		}
+
+		if (b > 255) {
+			b = 255;
+		}
+		if (b < 0) {
+			b = 0;
+		}
+
+		if (a > 255) {
+			a = 255;
+		}
+		if (a < 0) {
+			a = 0;
+		}
+
+		this.r = (byte) r;
+		this.g = (byte) g;
+		this.b = (byte) b;
+		this.a = (byte) a;
+	}
+
 	public boolean isDark() {
 		// make the bytes unsigned ints, as Java has no unsigned byte...
 		int intR = r & 0xFF;
