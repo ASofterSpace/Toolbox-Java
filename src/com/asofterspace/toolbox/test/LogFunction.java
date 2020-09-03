564310fe85b8ed257264941863cbd3c4108e383a
Tom Moya Schau <moya@asofterspace.com>
2020-09-03 10:22:38 +0200

    Add missing file

---
+++ src/com/asofterspace/toolbox/test/LogFunction.java
@@ -0,0 +1,10 @@
+/**
+ * Unlicensed code created by A Softer Space, 2020
+ * www.asofterspace.com/licenses/unlicense.txt
+ */
+package com.asofterspace.toolbox.test;
+
+public interface LogFunction {
+
+	void log(String logline);
+}
