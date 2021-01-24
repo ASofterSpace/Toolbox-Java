14b28c357f42c59a0b12d997a2a411a182a4d5c8
Tom Moya Schau <moya@asofterspace.com>
2021-01-24 20:39:09 +0100

    Simplify code

--- src/com/asofterspace/toolbox/web/WebServerAnswerInHtml.java
+++ src/com/asofterspace/toolbox/web/WebServerAnswerInHtml.java
@@ -4,8 +4,6 @@
  */
 package com.asofterspace.toolbox.web;
 
-import java.nio.charset.StandardCharsets;
-
 
 /**
  * This represents the html data that a web server might send as answer
@@ -13,37 +11,10 @@ import java.nio.charset.StandardCharsets;
  *
  * @author Tom Moya Schau, moya@asofterspace.com
  */
-public class WebServerAnswerInHtml implements WebServerAnswer {
-
-	private byte[] data;
-
+public class WebServerAnswerInHtml extends WebServerAnswerWithText {
 
 	public WebServerAnswerInHtml(String html) {
-		this.data = html.getBytes(StandardCharsets.UTF_8);
-	}
-
-	@Override
-	public long getContentLength() {
-
-		return data.length;
-	}
-
-	@Override
-	public String getPreferredCacheParadigm() {
-
-		// never keep html data as it might change all the time
-		return "no-store";
-	}
-
-	@Override
-	public String getContentType() {
-
-		return "text/html";
-	}
-
-	@Override
-	public byte[] getBinaryContent() {
-
-		return data;
+		super(html);
+		setTextKind("html");
 	}
 }
