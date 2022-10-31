7a1a42df6e4b9211c2411296fdbfb10284541683
Tom Moya Schau <moya@asofterspace.com>
2022-10-31 18:20:40 +0100

    add more HTML functions

--- src/com/asofterspace/toolbox/io/HTML.java
+++ src/com/asofterspace/toolbox/io/HTML.java
@@ -4,6 +4,8 @@
  */
 package com.asofterspace.toolbox.io;
 
+import com.asofterspace.toolbox.utils.StrUtils;
+
 
 public class HTML {
 
@@ -17,6 +19,47 @@ public class HTML {
 	 */
 	public static String escapeHTMLstr(Object strToEscape) {
 		return XML.escapeXMLstr(strToEscape);
+	}
+
+	/**
+	 * Takes in a string such as:
+	 *   foo < bar
+	 * Returns a string such as:
+	 * &nbsp;&nbsp;foo&nbsp;&lt;&nbsp;bar
+	 * That is, replaces all spaces with &nbsp;
+	 * @param str  a string that possibly contains <, >, etc. signs
+	 * @return a string in which all such signs are escaped
+	 */
+	public static String escapeHTMLstrNbsp(Object strToEscape) {
+		String escapedStr = escapeHTMLstr(strToEscape);
+		return StrUtils.replaceAll(escapedStr, " ", "&nbsp;");
+	}
+
+	/**
+	 * Takes in a string such as:
+	 *   foo < bar
+	 * Returns a string such as:
+	 * &nbsp;&nbsp;foo &lt; bar
+	 * That is, replaces front spaces with &nbsp;
+	 * @param str  a string that possibly contains <, >, etc. signs
+	 * @return a string in which all such signs are escaped
+	 */
+	public static String escapeHTMLstrNbspStart(Object strToEscape) {
+		String escapedStr = escapeHTMLstr(strToEscape);
+
+		// if nothing needs doing, do nothing
+		if (!escapedStr.startsWith(" ")) {
+			return escapedStr;
+		}
+
+		// if something needs doing, do that - that is, replace front spaces with &nbsp;
+		StringBuilder result = new StringBuilder();
+		while (escapedStr.startsWith(" ")) {
+			escapedStr = escapedStr.substring(1);
+			result.append("&nbsp;");
+		}
+		result.append(escapedStr);
+		return result.toString();
 	}
 
 	public static String unescapeHTMLstr(String strToUnescape) {
